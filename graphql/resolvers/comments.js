211ac3134bc03de49ff0dae1605542896d835104
NikhilT27 <nikhilthakare14@gmail.com>
2020-10-30 21:46:45 +0530

    add: apollo server functionalities

---
+++ graphql/resolvers/comments.js
@@ -0,0 +1,54 @@
+const {
+  UserInputError,
+  delegateToSchema,
+  AuthenticationError,
+} = require("apollo-server");
+
+const CheckAuth = require("../../util/check-auth");
+const Post = require("../../models/Post");
+
+module.exports = {
+  Mutation: {
+    async createComment(_, { postId, body }, context) {
+      const { username } = CheckAuth(context);
+      if (body.trim === "") {
+        throw new UserInputError("Comment can't be Empty", {
+          errors: {
+            body: "Comment body must not be empty",
+          },
+        });
+      }
+
+      const post = await Post.findById(postId);
+      if (post) {
+        post.comments.unshift({
+          body,
+          username,
+          createAt: new Date().toISOString(),
+        });
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post not found");
+    },
+
+    async deleteComment(_, { postId, commentId }, context) {
+      const { username } = CheckAuth(context);
+
+      const post = await Post.findById(postId);
+
+      if (post) {
+        const commentIndex = post.comments.findIndex((c) => c.id === commentId);
+
+        if (post.comments[commentIndex].username === username) {
+          post.comments.splice(commentIndex, 1);
+          await post.save();
+          return post;
+        } else {
+          throw new AuthenticationError("Action not allowed");
+        }
+      } else {
+        throw new UserInputError("Post not found");
+      }
+    },
+  },
+};
--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,7 +1,12 @@
 const postsResolvers = require("./posts");
 const usersResolvers = require("./users");
+const commentsResolvers = require("./comments");
 
 module.exports = {
+  Post: {
+    likeCount: (parent) => parent.likes.length,
+    commentCount: (parent) => parent.comments.length,
+  },
   Query: {
     ...postsResolvers.Query,
   },
@@ -9,5 +14,10 @@ module.exports = {
   Mutation: {
     ...usersResolvers.Mutation,
     ...postsResolvers.Mutation,
+    ...commentsResolvers.Mutation,
+  },
+
+  Subscription: {
+    ...postsResolvers.Subscription,
   },
 };
